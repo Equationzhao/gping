be1aa5888af642ac0586ae3733ebc5c15f2b99b1
equationzhao <equationzhao@foxmail.com>
2023-04-18 17:59:48 +0800

    update

--- cmd/root.go
+++ cmd/root.go
@@ -16,27 +16,25 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-const (
-	ttl      = 50
-	byteSize = 56
+var (
+	ttl      int
+	byteSize int
+	interval int
 )
 
 // Execute is the entry point of the program
-func Execute() {
+func Execute() error {
 	app := cli.NewApp()
 	app.Name = "gping"
 	app.UsageText = "gping [options] hosts..."
 	app.Usage = "a ping tool written in golang"
 	app.Version = "0.0.1"
-	var count int = 5
+	count := 65535
 	app.Flags = []cli.Flag{
-		&cli.BoolFlag{
-			Name:  "O",
-			Usage: "Also display a message if no response was received",
-		},
 		&cli.IntFlag{
-			Name:  "C",
-			Usage: "Ping a host only a specific number of times",
+			Name:    "c",
+			Aliases: []string{"count", "C"},
+			Usage:   "Ping a host only a specific number of times",
 			Action: func(context *cli.Context, u int) error {
 				if u > 65535 {
 					return errors.New("count should be less than 65535")
@@ -45,6 +43,45 @@ func Execute() {
 				return nil
 			},
 		},
+		&cli.IntFlag{
+			Name:        "byte-size",
+			Aliases:     []string{"bs"},
+			Usage:       "Set byte size of ping packet",
+			Value:       56,
+			Destination: &byteSize,
+		},
+		&cli.IntFlag{
+			Name:        "ttl",
+			Usage:       "Set ttl of ping packet",
+			Aliases:     []string{"TTL"},
+			Value:       50,
+			Destination: &ttl,
+		},
+		&cli.IntFlag{
+			Name:        "i",
+			Aliases:     []string{"I", "interval"},
+			Usage:       "specifying the interval in seconds between requests",
+			Value:       1,
+			Destination: &interval,
+		},
+		&cli.BoolFlag{
+			Name:    "6",
+			Aliases: []string{"ipv6"},
+			Usage:   "Use IPv6",
+			Value:   false,
+		},
+		&cli.BoolFlag{
+			Name:    "4",
+			Aliases: []string{"ipv4"},
+			Usage:   "Use IPv4",
+			Value:   false,
+		},
+		&cli.BoolFlag{
+			Name:    "a",
+			Aliases: []string{"bell"},
+			Usage:   "ring the bell when a packet is received (if your terminal supports it)",
+			Value:   false,
+		},
 	}
 	app.Action = func(c *cli.Context) error {
 		if c.NArg() == 0 {
@@ -52,43 +89,52 @@ func Execute() {
 			return nil
 		}
 
+		v4 := c.Bool("4")
+		v6 := c.Bool("6")
+
+		if v4 && v6 {
+			return errors.New("cannot use both -4 and -6")
+		}
+
 		hosts := c.Args().Slice()
-		validIP := make([]string, 0, len(hosts))
-		var err error
-		for _, host := range hosts {
-			// parse host into ip
-			ip, errTemp := net.ResolveIPAddr("ip", host)
-			if ip == nil {
-				tool.RedPrintln("ping: unknown host " + host)
-				err = errors.Join(err, errTemp)
-				continue
-			} else {
-				validIP = append(validIP, host)
-			}
-		}
-		l := len(validIP)
-		if l == 0 {
-			return cli.Exit(err, 1)
-		}
 
 		sig := make(chan os.Signal, 1)
 		signal.Notify(sig, os.Interrupt) // catch ctrl+c
+		l := len(hosts)
+		for i, v := range hosts {
 
-		for i, v := range validIP {
-			pinger, err := ping.NewPinger(v)
+			var ip *net.IPAddr
+			var err error
+			if v4 {
+				ip, err = net.ResolveIPAddr("ip4", v)
+			} else if v6 {
+				ip, err = net.ResolveIPAddr("ip6", v)
+			} else {
+				ip, err = net.ResolveIPAddr("ip", v)
+			}
+			if err != nil {
+				tool.RedPrintln(err)
+				continue
+			}
+
+			pinger := ping.New(ip.String())
 			pinger.Size = byteSize
 			pinger.TTL = ttl
-			tool.GreenPrintln("PING " + pinger.Addr() + " " + strconv.Itoa(pinger.Size) + " bytes of data.")
-			pinger.OnRecv = func(pkt *ping.Packet) {
-				tool.GreenPrintln(fmt.Sprintf("%d bytes from %s: icmp_seq=%d ttl=%d time=%dms", pkt.Nbytes, pkt.Addr, pkt.Seq+1, pinger.TTL, pkt.Rtt.Milliseconds()))
-			}
 			pinger.SetPrivileged(true)
-			if err != nil {
-				panic(err)
-			}
 			pinger.Count = count
-			pinger.Timeout = time.Second * 10 * time.Duration(count)
+			pinger.Interval = time.Duration(interval) * time.Second
+			pinger.Timeout = time.Duration(count) * time.Second * 10
+
+			Prompt := "PING " + v + " (" + ip.String() + ") " + strconv.Itoa(pinger.Size) + " bytes of data."
+
+			tool.GreenPrintln(Prompt)
 			start := time.Now()
+			pinger.OnRecv = func(pkt *ping.Packet) {
+				if c.Bool("a") {
+					fmt.Print("\x07")
+				}
+				tool.GreenPrintln(fmt.Sprintf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.3gms", pkt.Nbytes, pkt.Addr, pkt.Seq+1, pinger.TTL, float64(pkt.Rtt.Microseconds())/1000.0))
+			}
 
 			go func() {
 				<-sig
@@ -96,23 +142,25 @@ func Execute() {
 			}()
 
 			err = pinger.Run()
+
 			if err != nil {
-				panic(err)
+				tool.RedPrintln(err)
+				continue
 			}
 
-			statistic := pinger.Statistics()
-			res := statisticString(statistic, time.Since(start))
+			res := statisticString(pinger.Statistics(), time.Since(start))
 			fmt.Println()
 			tool.GreenPrintln(res)
 			if i != l-1 {
 				fmt.Println()
 			}
+
 		}
 		return nil
 	}
 	app.HideHelpCommand = true
 
-	app.Run(os.Args)
+	return app.Run(os.Args)
 }
 
 func statisticString(s *ping.Statistics, total time.Duration) string {
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/equationzhao/gping/cmd"
+import (
+	"github.com/equationzhao/gping/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	_ = cmd.Execute()
 }
